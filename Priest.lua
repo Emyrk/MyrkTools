075c54db27d37aee2abcb86d52323bb2c523ac4e
Emyrk <stevenmasley@gmail.com>
2025-08-17 22:20:56 -0500

    busy heals quit

--- Priest.lua
+++ Priest.lua
@@ -1,13 +1,37 @@
 MyrkPriest = {
 }
 
+MyrkPriest.manaThreshold = 0.60
+
 function MyrkPriest:PriestOffense()
+  local busy = QHExport.BusyQuickHeal()
+  if busy then
+    AutoMyrk:Warn("Quickheal busy " .. BoolToString(busy), 1, 1, 0)
+    return
+  end
+
+  if not HostileTarget() then
+    AutoMyrk:Warn("Not a hostile target ", 1, 1, 0)
+    -- UIErrorsFrame:AddMessage("Not a hostile target.",1,0.2,0.2); 
+    return
+  end
+
+  -- Start Attack if low on mana/shoot
+  -- If moving cast hot
+
   local m=UnitMana("player")/UnitManaMax("player");
-  if m>0.6 and UnitExists("target") and not UnitIsFriend("player","target") then 
+  if m>MyrkPriest.manaThreshold then 
     CastSpellByName("Smite"); 
+    AutoMyrk:Warn("Smite ", 1, 1, 0)
   else 
-    UIErrorsFrame:AddMessage("Not enough mana (>60%) or no hostile target.",1,0.2,0.2); 
+    AutoMyrk:Warn("Not enough mana ", 1, 1, 0)
   end
 end
 
 
+function MyrkPriest:QuickHeal()
+  if SlashCmdList.QUICKHEAL then
+    return SlashCmdList.QUICKHEAL("")
+  end
+  return false
+end
