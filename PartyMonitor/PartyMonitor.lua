ad480e9f8de1dfee48d014c271b820db1f91585d
Emyrk <stevenmasley@gmail.com>
2025-08-26 14:31:01 -0500

    comment

--- PartyMonitor/PartyMonitor.lua
+++ PartyMonitor/PartyMonitor.lua
@@ -1,3 +1,7 @@
+-- PartyMonitor keeps track of all the current members of our party including ourselves.
+-- We want to keep track of the incoming damage to each member. This will allow us to calculate
+-- the damage against them, and predict how much damage they will take in the future.
+-- This will all be used to predict healing.
 PartyMonitor = MyrkAddon:NewModule("MyrkPartyMonitor")
 
 function PartyMonitor:OnEnable()
